594c69737db9a16898bd17ef5caa2aae43aec0e1
Joe McTee <mcjoe@jeklsoft.com>
2019-10-27 21:19:39 -0600

    First commit, basic project

---
+++ src/main/java/scitech/Greeter.java
@@ -0,0 +1,20 @@
+package scitech;
+
+/**
+ *
+ * @author joemctee
+ */
+public class Greeter {
+
+    public String sayHello() {
+        return "Hello";
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/test/java/scitech/GreeterTest.java
@@ -0,0 +1,26 @@
+package scitech;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author joemctee
+ */
+public class GreeterTest {
+    private Greeter greeter;
+
+    @Before
+    public void setup() {
+        greeter = new Greeter();
+    }
+
+    @Test
+    public void testSayHello() throws Exception {
+        String result = greeter.sayHello();
+        
+        assertEquals("Did not receive expected greeting: " + result, "Hello", result);
+    }
+}
